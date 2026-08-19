b39972de1ab0bdf850f15b87d5a6cbbadd9c25a5
dory <78720364+dooory@users.noreply.github.com>
2026-08-19 18:29:02 +1200

    style: improve semantics

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,9 @@ function App() {
 
   return (
     <>
-      <h1>CV Application</h1>
+      <header>
+        <h1>CV Application</h1>
+      </header>
       <Form
         cv={cv}
         updateFormField={updateFormField}
